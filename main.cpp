9d184b7c1c36608424195793e68c258888d1a267
kikutani Takuma <k022g0126@g.neec.ac.jp>
2023-04-13 18:49:21 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <Windows.h>
+#include <cstdint>
+
+// ウィンドプロシージャ
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	switch (msg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+// Windowsアプリでのエントリーポイント
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	WNDCLASS wc{};
+	// ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
+
+	// ウィンドウクラス名
+	wc.lpszClassName = L"DirectXGame";
+
+	// インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+
+	// カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+	// ウィンドウクラスを登録
+	RegisterClass(&wc);
+
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
+
+	// ウィンドウサイズを表す構造体にクライアント領域を入れる
+	RECT wrc = { 0,0,kClientWidth,kClientHeight };
+
+
+	// クライアント領域を元に実際のサイズにwrcを変更してもらう
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+
+	// ウィンドウの生成
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,      // 利用するクラス名
+		L"DirectXGame",        // タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,   // よく見るウィンドウタイトル
+		CW_USEDEFAULT,         // 表示X座標(Windowsに任せる)
+		CW_USEDEFAULT,         // 表示Y座標(Windowsに任せる)
+		wrc.right - wrc.left,  // ウィンドウ横幅
+		wrc.bottom - wrc.top,  // ウィンドウ縦幅
+		nullptr,               // 親ウィンドウハンドル
+		nullptr,               // メニューハンドル
+		wc.hInstance,          // インスタンスハンドル
+		nullptr                // オプション
+	);
+
+	// ウィンドウを表示する
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg{};
+	// ウィンドウのxボタンが押されるまでループ
+	while (msg.message != WM_QUIT) {
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		} 
+		else {
+
+		}
+	}
+
+	OutputDebugStringA("Hello, DirectX\n");
+
+	return 0;
+}
